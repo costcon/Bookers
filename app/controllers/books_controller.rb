95d85f17a652d2257609a94acea5b64973b8a58a
costcon <tentekomambo@gmail.com>
2021-04-12 09:19:32 +0000

    add contents

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,8 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    @book = Book.all
+    @newbook = Book.new
   end
 
   def show
@@ -21,21 +22,21 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    @newbook = Book.new(book_params)
+    @newbook.save
+    redirect_to books_path(@newbook.id)
   end
 
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
     redirect_to book_path(book)
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
   end
 
